3194107693f3497a484df9c3344fbeaaec76a8e4
HasanMafatihul <16521045@mahasiswa.itb.ac.id>
2022-04-15 09:15:14 +0700

    Added tambah_game
    Signed-off-by: HasanMafatihul <16521045@mahasiswa.itb.ac.id>

---
+++ tambah_game.py
@@ -0,0 +1,24 @@
+questions = ["nama game", "kategori", "tahun rilis", "harga", "stok awal"]
+
+def banyak(li):
+    x = 0
+    for i in li:
+        x += 1
+    return x 
+
+# Menambah game
+def tambah_game(role, game_arr):
+    index = int(game_arr[banyak(game_arr)-1][0][4:7]) + 1
+    index = f'GAME{index:0>3}'
+    temp = game = [index]
+    while True:
+        for i in questions:
+            game = game + [input(f"Masukkan {i}: ")]
+        if "" in temp:
+            print("Mohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.")
+            game = temp
+        else:
+            break
+    game_arr = game_arr + game
+    print(f"Selamat! Berhasil menambahkan game {game[1]}.")
+    return game_arr
